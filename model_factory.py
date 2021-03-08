0724021c35869b683399041d57cb5eaa653d9024
Xinqiao zhang <30340321+xinqiaozhang@users.noreply.github.com>
2021-03-07 18:09:37 -0800

    Add files via upload

--- model_factory.py
+++ model_factory.py
@@ -9,6 +9,23 @@ import torch.nn as nn
 import torch
 import ssl
 
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+NUM_CLASSES = 2
+################################################################################
+# CSE 253: Programming Assignment 4
+# Code snippet by Ajit Kumar, Savyasachi
+# Fall 2020
+################################################################################
+import pdb
+from torchvision import models
+import torch.nn as nn
+import torch
+import ssl
+
 # Build and return the model here based on the configuration.
 def get_model(pretrained_model_type):
 
@@ -89,3 +106,236 @@ class DenseNet121Model(nn.Module):
         x = self.relu(x.view(x.shape[0], -1))
         x = self.fcLayer(x)
         return x
+
+
+class AlexNet(nn.Module):
+    """
+    Modified AlexNet for CIFAR
+    From: https://github.com/icpm/pytorch-cifar10/blob/master/models/AlexNet.py
+    """
+    def __init__(self, num_classes=NUM_CLASSES):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            # nn.Linear(256 * 2 * 2, 4096),
+            nn.Linear(16384, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        # import pdb; pdb.set_trace()
+        x = self.features(x)
+        # x = x.view(x.size(0), 256 * 2 * 2)
+        x = x.view(x.size(0), 16384)
+        x = self.classifier(x)
+        return x
+
+
+class Bottleneck(nn.Module):
+    """
+    Bottleneck module in DenseNet Arch.
+    See: https://arxiv.org/abs/1608.06993
+    """
+    def __init__(self, in_planes, growth_rate):
+        super(Bottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, 4 * growth_rate, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(4 * growth_rate)
+        self.conv2 = nn.Conv2d(4 * growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+
+    def forward(self, x):
+        y = self.conv1(F.relu(self.bn1(x)))
+        y = self.conv2(F.relu(self.bn2(y)))
+        x = torch.cat([y, x], 1)
+        return x
+
+
+class Transition(nn.Module):
+    """
+    Transition module in DenseNet Arch.
+    See: https://arxiv.org/abs/1608.06993
+    """
+    def __init__(self, in_planes, out_planes):
+        super(Transition, self).__init__()
+        self.bn = nn.BatchNorm2d(in_planes)
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        x = self.conv(F.relu(self.bn(x)))
+        x = F.avg_pool2d(x, 2)
+        return x
+
+
+class DenseNet(nn.Module):
+    """
+    From: https://github.com/icpm/pytorch-cifar10/blob/master/models/DenseNet.py
+    """
+    def __init__(self, block, num_block, growth_rate=12, reduction=0.5, num_classes=NUM_CLASSES):
+        super(DenseNet, self).__init__()
+        self.growth_rate = growth_rate
+
+        num_planes = 2 * growth_rate
+        self.conv1 = nn.Conv2d(3, num_planes, kernel_size=3, padding=1, bias=False)
+
+        self.dense1 = self._make_dense_layers(block, num_planes, num_block[0])
+        num_planes += num_block[0] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
+        self.trans1 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense2 = self._make_dense_layers(block, num_planes, num_block[1])
+        num_planes += num_block[1] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
+        self.trans2 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense3 = self._make_dense_layers(block, num_planes, num_block[2])
+        num_planes += num_block[2] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
+        self.trans3 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense4 = self._make_dense_layers(block, num_planes, num_block[3])
+        num_planes += num_block[3] * growth_rate
+
+        self.bn = nn.BatchNorm2d(num_planes)
+        self.linear = nn.Linear(num_planes, num_classes)
+
+    def _make_dense_layers(self, block, in_planes, num_block):
+        layers = []
+        for i in range(num_block):
+            layers.append(block(in_planes, self.growth_rate))
+            in_planes += self.growth_rate
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.trans1(self.dense1(x))
+        x = self.trans2(self.dense2(x))
+        x = self.trans3(self.dense3(x))
+        x = self.dense4(x)
+        x = F.avg_pool2d(F.relu(self.bn(x)), 4)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
+
+
+def DenseNet121():
+    return DenseNet(Bottleneck, [6, 12, 24, 16], growth_rate=32)
+
+
+def DenseNet169():
+    return DenseNet(Bottleneck, [6, 12, 32, 32], growth_rate=32)
+
+
+def DenseNet201():
+    return DenseNet(Bottleneck, [6, 12, 48, 32], growth_rate=32)
+
+
+def DenseNet161():
+    return DenseNet(Bottleneck, [6, 12, 36, 24], growth_rate=48)
+
+
+def densenet_cifar():
+    return DenseNet(Bottleneck, [6, 12, 24, 16], growth_rate=12)
+
+
+class ModdedLeNet5Net(nn.Module):
+    """
+    A modified LeNet architecture that seems to be easier to embed backdoors in than the network from the original
+    badnets paper
+    Input - (1 or 3)x28x28
+    C1 - 6@28x28 (5x5 kernel)
+    ReLU
+    S2 - 6@14x14 (2x2 kernel, stride 2) Subsampling
+    C3 - 16@10x10 (5x5 kernel)
+    ReLU
+    S4 - 16@5x5 (2x2 kernel, stride 2) Subsampling
+    C5 - 120@1x1 (5x5 kernel)
+    F6 - 84
+    ReLU
+    F7 - 10 (Output)
+    """
+
+    def __init__(self, channels=1):
+        super(ModdedLeNet5Net, self).__init__()
+        self.convnet = nn.Sequential(
+            nn.Conv2d(channels, 6, kernel_size=(1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(6, 16, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(16, 120, kernel_size=(5, 5)),
+            nn.ReLU()
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 2),
+            nn.LogSoftmax(dim=-1)
+        )
+
+    def forward(self, img):
+        output = self.convnet(img)
+        output = output.view(img.size(0), -1)
+        output = self.fc(output)
+        return output
+
+
+class BadNet(nn.Module):
+    """
+    Mnist network from BadNets paper
+    Input - 1x28x28
+    C1 - 1x28x28 (5x5 kernel) -> 16x24x24
+    ReLU
+    S2 - 16x24x24 (2x2 kernel, stride 2) Subsampling -> 16x12x12
+    C3 - 16x12x12 (5x5 kernel) -> 32x8x8
+    ReLU
+    S4 - 32x8x8 (2x2 kernel, stride 2) Subsampling -> 32x4x4
+    F6 - 512 -> 512
+    tanh
+    F7 - 512 -> 10 Softmax (Output)
+    """
+
+    def __init__(self):
+        super(BadNet, self).__init__()
+        self.convnet = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(16, 32, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=(2, 2), stride=2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 2),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, img):
+        output = self.convnet(img)
+        output = output.view(img.size(0), -1)
+        output = self.fc(output)
+        return output
